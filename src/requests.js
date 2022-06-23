7e1d338a6e618113fbc6425684b4f3116fbfb7e4
Kush Goyal <82465969+kushgoyal12@users.noreply.github.com>
2022-06-23 20:14:51 +0530

    first commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "f80dd9f1c67fc8f3c5ef762ae7c0861b";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default requests;
