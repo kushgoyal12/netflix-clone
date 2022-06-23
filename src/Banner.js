c9004b0692e1afa8424c017b7228645983e0ff85
Kush Goyal <82465969+kushgoyal12@users.noreply.github.com>
2022-06-23 21:00:42 +0530

    fixed banner position

--- src/Banner.js
+++ src/Banner.js
@@ -26,7 +26,7 @@ function Banner() {
             style={{
                 backgroundSize: "cover",
                 backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-                backgroundPosition: "center center",
+                backgroundPosition: "top",
             }}
         >
             <div className='banner__contents'>
